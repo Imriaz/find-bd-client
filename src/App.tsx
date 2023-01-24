b682423a1eaa421f0d8f51fe93c6ec78a4c67955
Imriaz <imriaz.cse.bubt@gmail.com>
2023-01-24 14:57:53 +0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Edit by Imriaz <code>src/App.tsx</code> and save to reload.
+          Hello Edit by Imriaz Uddin <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
